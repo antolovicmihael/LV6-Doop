fc2692eee3105beeef5a7620099e98836d411765
MIHAEL ANTOLOVIC <antolovicmihael@gmail.com>
2020-05-20 19:47:43 +0200

    Zadatak 3

---
+++ LV6 Zad3/LV6 Zad3/CareTaker.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LV6_Zad3
+{
+    class CareTaker
+    {
+        public Memento PreviousState { get; set; }
+        Stack<Memento> stack = new Stack<Memento>();
+
+        public void PushStack(Memento memento)
+        {
+            stack.Push(memento);
+        }
+        public Memento PopStack()
+        {
+            return stack.Pop();
+        }
+
+    }
+}
---
+++ LV6 Zad3/LV6 Zad3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LV6_Zad3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var date = new DateTime(2020, 5, 20, 19, 45, 36); //year,month,day,hour,minute,second
+
+            ToDoItem item = new ToDoItem("M", "A", date);
+            Console.WriteLine(item.ToString());
+            CareTaker careTaker = new CareTaker();
+            careTaker.PushStack(item.StoreState());
+            item.Rename("Mihael");
+            item.ChangeTask("Antolovic");
+            Console.WriteLine(item.ToString());
+            item.RestoreState(careTaker.PopStack());
+            Console.WriteLine(item.ToString());
+        }
+    }
+}
