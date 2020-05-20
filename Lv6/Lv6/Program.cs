2325662b1aa648b0ef4690c77f3cacc2d214c5df
MIHAEL ANTOLOVIC <antolovicmihael@gmail.com>
2020-05-20 19:28:12 +0200

    Zadatak 1

---
+++ Lv6/Lv6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Lv6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Notebook notebook = new Notebook();
+            Note noteforremoval = null;
+
+            for (int i = 0; i < 5; i++)
+            {
+
+                Note note = new Note("title" + i, "text" + i);
+                notebook.AddNote(note);
+                if (i == 3)
+                {
+                    noteforremoval = note;
+                }
+
+            }
+            notebook.RemoveNote(noteforremoval);
+
+        }
+    }
+}
